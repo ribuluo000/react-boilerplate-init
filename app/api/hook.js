4897735a28672fdb5ae9a055691a24d4d3850209
Nick <ribuluo000@163.com>
2020-03-03 22:36:48 +0800

    接口调用 封装 hook useApi Demo;

---
+++ app/api/hook.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-return-await */
+import React, { useState, useEffect } from 'react';
+import * as api from 'api';
+
+export const useApi = () => {
+  const [requestData, setRequestData] = useState({
+    funcName: '',
+    options: {},
+    startTime: '',
+  });
+  const { funcName, options, startTime, } = requestData;
+  const initialState = {
+    isLoading: false,
+    isError: false,
+    data: null,
+  };
+  const [state, setState] = useState(initialState);
+  useEffect(() => {
+    let didCancel = false;
+    const fetchData = async () => {
+      setState({ ...initialState, isLoading: true });
+      try {
+        const jsonObj = await api[funcName](options);
+        if (didCancel) {
+          return;
+        }
+        setState({ ...initialState, data: jsonObj.data });
+      } catch (error) {
+        if (didCancel) {
+          return;
+        }
+        setState({ ...initialState, isError: true });
+      }
+    };
+    if (funcName) {
+      fetchData();
+    }
+    return () => {
+      didCancel = true;
+    };
+  }, [funcName, options, startTime]);
+  return [state, setRequestData];
+};
